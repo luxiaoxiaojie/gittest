714d82c44100c618680deee9b4aefd9a0eb6a537
xjlu <13228803516@163.com>
2020-05-04 02:58:14 +0800

    no message

---
+++ jekins/src/test/java/com/jenkinstest/jekins/JekinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jenkinstest.jekins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JekinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
